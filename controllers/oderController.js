76ce497e4fcfdfc324d778177ae96bf8f1f5d966
levanquyet11099 <101162799+levanquyet11099@users.noreply.github.com>
2023-12-13 12:26:55 +0700

    cap nhat tinh tong tien ban va so luong mon

--- controllers/oderController.js
+++ controllers/oderController.js
@@ -26,21 +26,13 @@ module.exports.getTableproductlistt = async (req, res) => {
       const tableId = req.params.id_table;
      if(!tableId){
        tableId = req.body.id_table;
-       console.log("met lam roi day ",tableId);
      }
-      
       const tables = await Table.findOne({id_table:tableId });
-     console.log("::::",tables,"::::");
+      
       const products  = await Product.find();
      
       const tables2 = await TableProduct.findOne({id_table:tableId });
-      console.log("::::",tables2,"::::");
-      if (tables2 && (tables2.id_product === null || tables2.id_table === null)) {
-        console.log("Có giá trị null trong đối tượng tables2.");
-    } else {
-        console.error("Không có giá trị null trong đối tượng tables2.");
-    }
-
+      
       res.render('../views/oder/odertable', { tables, user, products,tables2 });
     } catch (error) {
       console.log(error);
@@ -55,84 +47,34 @@ module.exports.getTableproductlistt = async (req, res) => {
       if(!user){
         res.redirect('/login');
       }
-      const tableId = req.body.id_table;
-      const productId = req.body.id_product;
-     // console.log("table : ",tableId, "- productId: ",productId);
+      const  id_table = req.body.id_table;
+      const id_product =req.body.id_product;
+      const tableId = id_table
+      const productId = id_product;
+     
       if(productId){
-        const nameproduct = await Product.findOne({id_product: productId});
-        //console.log("nameproduct.name::",nameproduct.name);
-        const nameproduct1 = nameproduct.name;
-        //console.log("table : ",tableId, "- productId: ",productId);
-  
-        //lưu sản phẩm khi thêm và tăng số lượng trên bàn đó nếu có 
-        const tableProducts1 = await TableProduct.findOne({id_table: tableId, id_product: productId} );
-        if(tableProducts1){
-          tableProducts1.quantity += 1;
-         
-          tableProducts1.nameproduct = nameproduct.name;
-            await tableProducts1.save();
-            //console.log("da save +1");
-        }else{
-            const tableProducts1  = new TableProduct({ id_table: tableId, id_product: productId, quantity : 1, Status : "1" , nameproduct: nameproduct1 });
-            await tableProducts1 .save();
-            //console.log("da save",tableProducts1 );
-        }
-        //lấy tất cả sp theo bàn
+        const newTableProduct = await TableProduct.saveTableProduct(tableId,  productId);   
         const tableProducts = await TableProduct.find({ id_table: tableId });
-        //console.log("tableProducts::",tableProducts);
-       // Tạo một Set để lưu trữ các id sản phẩm duy nhất
-       const uniqueProductIds = new Set(tableProducts.map(item => item.id_product));
-        
-       const products1 = await Product.find({ id_product: { $in: Array.from(uniqueProductIds) } });
-         // console.log(tableProducts);
-          let totalPrice = 0;
-          let quantityvalue = 0;
-          let productname = "";
-          const productQuantities = {};
-          tableProducts.forEach(tableProduct => {
-           // console.log("tableProduct.id_product: ::::::",tableProduct.id_product);
-            const product = products1.find(product => product.id_product === tableProduct.id_product);
-            //console.log("product:", product);
-            if (product) {
-              const quantity = tableProduct.quantity || 0;
-              //console.log("quantity:",quantity);
-              const productPrice = product.price || 0;
-             // console.log("productPrice:",productPrice);
-              totalPrice += productPrice * quantity;
-              //console.log("total:",totalPrice);
-              // Đếm số lượng mỗi sản phẩm
-              productname = product.name;
-              productQuantities[product.name] = (productQuantities[product.name] || 0) + quantity;
-              //console.log("quantity:",productQuantities);
-              quantityvalue =  productQuantities[product.name];
-              //console.log("quantity:",quantityvalue);
-            }
-          });
-          const result = {
-            totalPrice,
-            productQuantities,
-            quantityvalue,
-          };
-          //console.log("san pham",result);
-          //console.log("so luong san pham::",quantityvalue);
-          
+        let totalPriceForTable = 0;
+        let quantityTable =0;
+        tableProducts.forEach((tableProduct) => {
+            // Cộng giá trị total của từng tableProduct vào tổng giá trị của bàn
+            totalPriceForTable += tableProduct.total;
+            quantityTable += tableProduct.quantity;
+        });
           tables1 = await Table.updateOne(
             { id_table: tableId }, 
             {  $set: {
-              total: totalPrice,
-              quantity: quantityvalue,
-              nameproduct : productname,
+              total: totalPriceForTable,
+              quantity: quantityTable,
             }, } ,
-          
           );
           const tables = await Table.find({id_table: tableId});
-          //console.log("bàn tables::::",tables);
-          const tables2 = await TableProduct.find({id_table: tableId});
-          console.log("bàn tables::::",tables2," end tables2");
+         
+          let tables2 = await TableProduct.find({id_table: tableId});
           tables2 = tables2.filter(
             (record) => record.id_product !== null && record.id_table !== null
           );
-          console.log("tại sao");
           const products = await Product.find();
           res.render('../views/oder/odertable', {tables, user, products,tables2 });
       }else{
@@ -140,11 +82,8 @@ module.exports.getTableproductlistt = async (req, res) => {
           const products = await Product.find();
           res.render('../views/oder/odertable', {tables, user, products});
       }
-      
-
     }catch(error){
       console.log(error);
       res.status(500).json({ error: 'Error' });
     }
-
   };
--- models/TableProduct.js
+++ models/TableProduct.js
@@ -1,33 +1,46 @@
 const mongoose = require('mongoose');
+const Product = require('./product');
+
 
 const tableProductSchema = new mongoose.Schema({
     id_table: { type: String, ref: 'Table' },
     id_product: { type: String, ref: 'Product' },
+    nameproduct: String,
     quantity: Number,
     Status: String,
     price: Number,
     total: Number,
     name: String,
-    nameproduct: String,
+
     
    
 });
 
-tableProductSchema.statics.saveTableProduct = async function (id_table, id_product) {
+tableProductSchema.statics.saveTableProduct = async function (tableId, productId) {
     try {
-        const exists = await TableProduct.findOne({id_table, id_product} );
-        if(exists){
-            exists.quantity += 1;
-            await exists.save();
+        const nameproduct1 = await Product.findOne({id_product: productId});
+        const newTableProduct = await TableProduct.findOne({id_table: tableId, id_product: productId} );
+        if(newTableProduct){
+          newTableProduct.quantity += 1;
+          newTableProduct.nameproduct = nameproduct1.name;
+          newTableProduct.price = nameproduct1.price;
+          newTableProduct.total = newTableProduct.quantity * newTableProduct.price;
+            await newTableProduct.save();
+            console.log("save +1 ", newTableProduct);
+            return newTableProduct;
         }else{
-            const newRecord  = new this({ id_table, id_product, quantity : 1  });
-            await newRecord .save();
+          console.log("table:",{tableId});
+         
+            const  newTableProduct = new TableProduct({ id_table: tableId, id_product: productId, quantity : 1 , Status : "1" , nameproduct : nameproduct1.name, price: nameproduct1.price, total : nameproduct1.price });
+            await newTableProduct.save();
+            console.log("save =1", newTableProduct);
+            return newTableProduct;
         }
-      return newTableProduct;
     } catch (error) {
       throw error;
     }
   };
+
 
 const TableProduct = mongoose.model('tableproduct', tableProductSchema);
 
